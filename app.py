1d4b0cca755647ee65e0d07cae91df9dedbb2352
Stasys Civilis <scivitas@gmail.com>
2022-02-23 18:34:10 +0200

    Pridetas app file

---
+++ app.py
@@ -0,0 +1,28 @@
+import os.path
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    __tablename__ = "messages"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    message = db.Column(db.Text, nullable=False)
+
+    def __init__(self, name, email, message):
+        self.name = name
+        self.email = email
+        self.message = message
+
+    def __repr__(self):
+        return f'{self.name} - {self.email}'
